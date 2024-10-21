93243b0eaec8c89d0a077c5a54d023ba605c06d7
Jacobs Agyei <jacobs.agyei@amalitech.com>
2024-10-21 11:57:39 +0000

    initial commit

---
+++ src/main/java/dev/aries/iijra/IijraBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.aries.iijra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IijraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IijraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aries/iijra/IijraBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.aries.iijra;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IijraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
