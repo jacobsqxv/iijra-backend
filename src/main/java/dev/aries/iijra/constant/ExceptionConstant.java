cc816958971653de969fe2ef568ad342b5b687a7
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-23 16:15:45 +0000

    feat: update exception constants

--- src/main/java/dev/aries/iijra/constant/ExceptionConstant.java
+++ src/main/java/dev/aries/iijra/constant/ExceptionConstant.java
@@ -1,6 +1,7 @@
 package dev.aries.iijra.constant;
 
 public class ExceptionConstant {
+
 	private ExceptionConstant() {
 	}
 
@@ -10,4 +11,9 @@ public class ExceptionConstant {
 	public static final String STAFF_ID_DOESNT_EXIST = "Staff does not exist with id: ";
 	public static final String STAFF_EMAIL_ALREADY_EXISTS = "Staff already exists with name: ";
 	public static final String STAFF_EMAIL_DOESNT_EXIST = "Staff does not exist with email: ";
+	public static final String TOKEN_VALUE_DOESNT_EXIST = "Token does not exist with value: ";
+	public static final String TOKEN_EXPIRED = "Token has expired";
+	public static final String PASSWORD_ALREADY_USED = "Password already in use";
+	public static final String ACCOUNT_DISABLED = "Cannot log in at this time, account has been disabled";
+	public static final String INVALID_REQUEST = "There was a problem with the request";
 }
