a24850bbd88ee5eabac14d15e80ed88375b79cc9
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-24 20:18:51 +0000

    feat: update security configuration

--- src/main/java/dev/aries/iijra/security/JwtService.java
+++ src/main/java/dev/aries/iijra/security/JwtService.java
@@ -3,17 +3,13 @@ package dev.aries.iijra.security;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -24,7 +20,6 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
 	private final JwtEncoder encoder;
-	private final JwtDecoder decoder;
 
 	public String generateToken(Authentication auth) {
 		Instant now = Instant.now();
@@ -41,12 +36,4 @@ public class JwtService {
 		return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
 	}
 
-//	public UUID validateJwt(String token) {
-//		Jwt jwt = decoder.decode(token);
-//		Instant expiry = Objects.requireNonNull(jwt.getExpiresAt());
-//		if (expiry.isAfter(Instant.now())) {
-//			throw new IllegalArgumentException("Token expired");
-//		}
-//		return UUID.fromString(jwt.getSubject());
-//	}
 }
