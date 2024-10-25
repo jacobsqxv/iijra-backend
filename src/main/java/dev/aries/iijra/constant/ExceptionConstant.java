4181acc6151c125e8865c36a249e092e0c428b44
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-25 15:10:09 +0000

    refactor: update constants

--- src/main/java/dev/aries/iijra/constant/ExceptionConstant.java
+++ src/main/java/dev/aries/iijra/constant/ExceptionConstant.java
@@ -1,19 +1,24 @@
 package dev.aries.iijra.constant;
 
-public class ExceptionConstant {
+public final class ExceptionConstant {
+
+	public static final String DEPT_NAME_ALREADY_EXISTS = "Department already exists with name: ";
+	public static final String STAFF_EMAIL_ALREADY_EXISTS = "Staff already exists with name: ";
+	public static final String PASSWORD_ALREADY_USED = "Password already in use";
+
+	public static final String DEPT_ID_DOESNT_EXIST = "Department does not exist with id: ";
+	public static final String DEPT_NAME_DOESNT_EXIST = "Department does not exist with name: ";
+	public static final String STAFF_ID_DOESNT_EXIST = "Staff does not exist with id: ";
+	public static final String STAFF_EMAIL_DOESNT_EXIST = "Staff does not exist with email: ";
+	public static final String TOKEN_VALUE_DOESNT_EXIST = "Token does not exist with value: ";
+
+	public static final String ACCOUNT_DISABLED = "Cannot log in at this time, account has been disabled";
+	public static final String ACCOUNT_NOT_ACTIVATED = "Cannot log in at this time, account has not been activated";
+	public static final String INVALID_REQUEST = "There was a problem with the request";
+	public static final String ACCESS_DENIED = "Access denied: Not authorized to access resource";
+	public static final String INVALID_CREDENTIALS = "Authentication failed: Invalid credentials";
+	public static final String INVALID_TOKEN = "Token is invalid or has expired";
 
 	private ExceptionConstant() {
 	}
-
-	public static final String DEPT_ID_DOESNT_EXIST = "Department does not exist with id: ";
-	public static final String DEPT_NAME_ALREADY_EXISTS = "Department already exists with name: ";
-	public static final String DEPT_NAME_DOESNT_EXIST = "Department does not exist with name: ";
-	public static final String STAFF_ID_DOESNT_EXIST = "Staff does not exist with id: ";
-	public static final String STAFF_EMAIL_ALREADY_EXISTS = "Staff already exists with name: ";
-	public static final String STAFF_EMAIL_DOESNT_EXIST = "Staff does not exist with email: ";
-	public static final String TOKEN_VALUE_DOESNT_EXIST = "Token does not exist with value: ";
-	public static final String TOKEN_EXPIRED = "Token has expired";
-	public static final String PASSWORD_ALREADY_USED = "Password already in use";
-	public static final String ACCOUNT_DISABLED = "Cannot log in at this time, account has been disabled";
-	public static final String INVALID_REQUEST = "There was a problem with the request";
 }
