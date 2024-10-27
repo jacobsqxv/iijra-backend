b32d4e68f245215f6ce9e6781a0930f334631439
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-27 13:11:17 +0000

    feat: update dto validation

--- src/main/java/dev/aries/iijra/module/staff/StaffRequest.java
+++ src/main/java/dev/aries/iijra/module/staff/StaffRequest.java
@@ -1,15 +1,16 @@
 package dev.aries.iijra.module.staff;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record StaffRequest(
 		@NotBlank(message = "Email is required")
 		String email,
 		@NotBlank(message = "Email is required")
 		String fullName,
-		@NotBlank(message = "Email is required")
+		@NotNull(message = "Email is required")
 		Long departmentId,
-		@NotBlank(message = "Email is required")
+		@NotNull(message = "Email is required")
 		Boolean isHod
 ) {
 }
