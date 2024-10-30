b0608956f4891cd9515d78d4c4ce0541fc097155
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-30 13:12:07 +0000

    feat: add default profile picture for admins

--- src/main/java/dev/aries/iijra/module/admin/AdminService.java
+++ src/main/java/dev/aries/iijra/module/admin/AdminService.java
@@ -6,6 +6,7 @@ import dev.aries.iijra.module.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,11 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminService {
 	private final UserService userService;
 	private final AdminRepository adminRepo;
+	@Value("${user.default-profile-image}")
+	private String defaultProfileImage;
 
 	@Transactional
 	public AdminResponse addNewSystemAdmin(AdminRequest request) {
 		User user = userService.createUser(request.email(), Role.SYS_ADMIN);
 		Admin newAdmin = new Admin(user, request.fullName());
+		newAdmin.setProfileImage(defaultProfileImage);
 		adminRepo.save(newAdmin);
 		return AdminResponse.newResponse(newAdmin);
 	}
