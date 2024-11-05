2b2f9c70eac5d37ebfe76ac4278b890457bb390a
Jacobs Agyei <agyeijacobs@outlook.com>
2024-11-05 14:23:38 +0000

    refactor: remove entity graph from query

--- src/main/java/dev/aries/iijra/module/department/DepartmentRepository.java
+++ src/main/java/dev/aries/iijra/module/department/DepartmentRepository.java
@@ -3,13 +3,11 @@ package dev.aries.iijra.module.department;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 	boolean existsByName(String name);
 
-	@EntityGraph(attributePaths = {"hod", "staff"})
 	List<Department> findByIsArchivedFalse();
 
 	int deleteByIsArchivedTrueAndArchivedAtBefore(LocalDateTime cutOff);
